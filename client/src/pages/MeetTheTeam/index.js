1bf3286f6d45bc0116b3b242357f11e8e8ea792b
Rachel Levy <rwillowlevy@gmail.com>
2020-07-21 17:08:44 -0500

    alex information

--- client/src/pages/MeetTheTeam/index.js
+++ client/src/pages/MeetTheTeam/index.js
@@ -9,9 +9,21 @@ function MeetTheTeam () {
         <Row>
             <Col s={12} m={6}>
                 <h1>Alex Bailon</h1>
-                <img src=''/>
+                <img src='https://avatars2.githubusercontent.com/u/59755671?s=460&u=283873d7bfe90d9df1d3dd5afd5ed79d23e9c73e&v=4'/>
                 <p>
-
+                Full-Stack Web Developer with a background in customer service ensuring 
+                that customers are always fully satisfied with the product and service 
+                they receive. Recently earned a certificate in Full Stack  Development 
+                from Northwestern University, with developed skills in JavaScript, node.js,  
+                React.js, and responsive web design. Having analytical problem solving skills 
+                and adaptableness, works alongside a team to efficiently create and develop 
+                mobile-first applications.
+                </p>
+                <p>
+                    LinkedIn: <a href="https://www.linkedin.com/in/alex-bailon/">Alex Bailon</a>
+                </p>
+                <p>
+                    GitHub: <a href="https://github.com/Alex-Bailon">Alex-Bailon</a>
                 </p>
             </Col>
             <Col s={12} m={6}>

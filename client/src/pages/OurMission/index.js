e656a7eb45fc49090cda55a33a0cead0adeb133c
Rachel Levy <rwillowlevy@gmail.com>
2020-07-21 16:40:25 -0500

    outline for our mission

---
+++ client/src/pages/OurMission/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-materialize'
+import 'materialize-css'
+
+function MeetTheTeam () {
+  return (
+    <Container>
+        <h1>Our Mission</h1>
+        <p>
+            
+        </p>
+    </Container>
+  )
+}
+
+export default MeetTheTeam

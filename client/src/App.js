880cdb01655b5beaea3e526d9ed3f6e0f74beb19
Rachel Levy <rwillowlevy@gmail.com>
2020-07-21 16:42:52 -0500

    created routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@ import Login from "./pages/Login/index"
 import Home from './pages/Home/index'
 import Profile from './pages/Profile'
 import Messages from './pages/Messages'
+import OurMission from './pages/OurMission'
+import MettTheTeam from './pages/MeetTheTeam'
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from './components/PrivateRoute'
 import './style.css'
+import MeetTheTeam from "./pages/MeetTheTeam";
 
 function App() {
   return (
@@ -16,6 +19,8 @@ function App() {
       <Nav />
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route exact path="/ourmission" component={OurMission} />
+        <Route exact path="/meettheteam" component={MeetTheTeam} />
         <PrivateRoute path="/match">
           <Route exact path="/match" component={Home} /> 
         </PrivateRoute >
